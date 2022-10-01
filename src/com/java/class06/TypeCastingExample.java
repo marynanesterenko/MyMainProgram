4b55576ef98ed8f1037e9781b0b32352e1c695e9
marynanesterenko <marynanesterenko95@gmail.com>
2022-10-01 14:35:53 -0400

    SecondCommit_Updated xml file name

--- src/com/java/class06/TypeCastingExample.java
+++ src/com/java/class06/TypeCastingExample.java
@@ -1,6 +1,6 @@
 package com.java.class06;
 
-public class TypeCastingExample {
+public class TypeCastingExample { // this is called class header, and that is where we declare a class
     public static void main(String[] args) {
         double a = 10, b = 20;
         int result;
