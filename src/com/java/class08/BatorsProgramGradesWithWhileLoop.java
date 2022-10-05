7406794848ea831e2cfbd8a83ce9e2a4e7b77166
marynanesterenko <marynanesterenko95@gmail.com>
2022-10-05 02:41:13 -0400

    SecondCommit

---
+++ src/com/java/class08/BatorsProgramGradesWithWhileLoop.java
@@ -0,0 +1,29 @@
+package com.java.class08;
+
+import java.util.Scanner;
+
+public class BatorsProgramGradesWithWhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Pls enter mark: ");
+        int x = sc.nextInt();
+        while (x > 100 || x < 0) {
+            System.out.println("Error!!! pls enter between 0-100");
+            x = sc.nextInt();
+        } if (x >= 90) {
+            System.out.println("A+");
+        } else if (x >= 80) {
+            System.out.println("A");
+        } else if (x >= 70) {
+            System.out.println("B+");
+        } else if (x >= 60) {
+            System.out.println("B");
+        } else if (x >= 50) {
+            System.out.println("C+");
+        } else if (x >= 40) {
+            System.out.println("C");
+        } else if (x >= 0) {
+            System.out.println("Fail");
+        }
+    }
+}
