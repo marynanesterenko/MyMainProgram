64731da63192fa13767d2cbb68326fc773d77682
marynanesterenko <marynanesterenko95@gmail.com>
2022-11-15 03:36:33 -0500

    New practice added

--- src/com/java/class25_Nov14/Homework/MainClass.java
+++ src/com/java/class25_Nov14/Homework/MainClass.java
@@ -17,13 +17,12 @@ public class MainClass {
         cellPhone.model = "Plus";
         cellPhone.fullName(cellPhone.manufacturer, cellPhone.name, cellPhone.version, cellPhone.model);
 
-        // calling the method to calculate the monthly price for us
-
+        // calling the method to display the storage options for the customer
         cellPhone.displayFullPriceBasedOnStorage(cellPhone.storages);
 
+        // calling the method to calculate the monthly price for us
         cellPhone.isContract = true;
         cellPhone.currency = '$';
-
         System.out.println("\nPlease enter the desired storage for your device: ");
         cellPhone.desiredStorage = sc.nextInt();
         cellPhone.calculatePricePerMonth(true, cellPhone.desiredStorage );
