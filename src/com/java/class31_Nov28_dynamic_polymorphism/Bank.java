d280978c9320b12a5d69b3f92fce8f19faf5cbb3
marynanesterenko <marynanesterenko95@gmail.com>
2022-11-28 20:34:40 -0500

    Updates made to the code

--- src/com/java/class31_Nov28_dynamic_polymorphism/Bank.java
+++ src/com/java/class31_Nov28_dynamic_polymorphism/Bank.java
@@ -11,18 +11,21 @@ public class Bank {
         System.out.println("Available balance - " + balance);
     }
 
+    // we need this method in the parent class, so that we can call it from PayPal
     public void displayRateOfInterest(){
         System.out.println("3.0");
     }
 }
 
 class ChaseBank extends Bank{
+    // method gets overridden, because the sub-class wants to utilize it in it's own way
     public void displayRateOfInterest(){
         System.out.println("3.0");
     }
 }
 
 class BankOfAmerica extends Bank {
+    // method gets overridden
     public void displayRateOfInterest(){
         System.out.println("3.0");
     }
@@ -30,6 +33,7 @@ class BankOfAmerica extends Bank {
 }
 
 class WellsFargo extends Bank {
+    // method gets overridden
     public void displayRateOfInterest(){
         System.out.println("3.0");
     }
--- src/com/java/class31_Nov28_dynamic_polymorphism/PayPal.java
+++ src/com/java/class31_Nov28_dynamic_polymorphism/PayPal.java
@@ -1,4 +1,16 @@
 package com.java.class31_Nov28_dynamic_polymorphism;
+// PayPal
 
 public class PayPal {
+    void checkBalance(Bank b){
+        b.checkBalance();
+    }
+
+    void displayInterest(Bank b){
+        b.displayRateOfInterest();
+    }
+
+    void withdraw (Bank b, int amount){
+        b.withdraw(amount);
+    }
 }
