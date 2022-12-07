9d6e779d4f6d04612543a35b6809098707562a3a
marynanesterenko <marynanesterenko95@gmail.com>
2022-12-06 19:05:48 -0500

    Updated Practice

---
+++ src/com/java/class32_Nov29_encapsulation_getters_setters/EncapsulationConcept.java
@@ -0,0 +1,2 @@
+package com.java.class32_Nov29_encapsulation_getters_setters;public class EncapsulationConcept {
+}
---
+++ src/com/java/class34_Dec5_Abstarction/TouchIDIphones.java
@@ -0,0 +1,2 @@
+package com.java.class34_Dec5_Abstarction;public class TouchIDIphones {
+}
